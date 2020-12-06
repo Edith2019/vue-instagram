c8c944a4b9ad80e79145598d1bfdf18141d298a7
Edith <edith.chevallier@hotmail.fr>
2020-12-06 13:34:08 +0100

    Overall set up

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container id="main" fluid>
+        <Users />
+        <!-- <Filters /> -->
+        <Search />
+    </v-container>
+</template>
+
+<script>
+import Users from "./Users";
+// import Filters from "./Filters";
+import Search from "./Search";
+
+export default {
+    name: "Main",
+    components: {
+        Users,
+        // Filters,
+        Search,
+    },
+
+    data: () => ({}),
+};
+</script>
---
+++ src/components/Users.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <p>Select the user you want to see the feed</p>
+        <v-chip v-for="user in users" :key="user">
+            {{ user }}
+        </v-chip>
+    </v-container>
+</template>
+
+<script>
+import { USERS } from "../consts";
+export default {
+    name: "Users",
+    data() {
+        return {
+            users: USERS,
+        };
+    },
+};
+</script>
