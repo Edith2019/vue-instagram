e048c9635dbe8a5ce92f60fc15707c267214a2ee
Edith <edith.chevallier@hotmail.fr>
2020-12-06 22:17:43 +0100

    cards

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,4 +1,5 @@
 export const DEFAULT_USER = 'artnightevents';
 export const USERS = [DEFAULT_USER, 'shakenightevents', 'plantnightevents'];
 export const ENDPOINT = "https://www.instagram.com/:username/?__a=1";
+export const MAX_CAPTION_LENGTH = 150;
 console.log(ENDPOINT)
--- src/services/data.service.js
+++ src/services/data.service.js
@@ -1,9 +1,29 @@
-import { ENDPOINT } from "../consts"
+import { ENDPOINT, MAX_CAPTION_LENGTH } from "../consts"
 
+
+
+const minimizeCaption = (caption) => {
+    console.log("caption", caption)
+    const length = caption.lenght;
+    console.log("text", length)
+    return length > MAX_CAPTION_LENGTH ? `${caption.substring(0, MAX_CAPTION_LENGTH)}...` : caption;
+}
+
+console.log(minimizeCaption)
+
+const minimizeVideosData = (videos) => {
+    const { edges } = videos
+    console.log("edges in video", edges)
+}
+
+console.log(minimizeVideosData)
 const getFeedsFromResponse = (response = {}) => {
     const {
         graphql: { user },
     } = response;
+
+
+    console.log("response graphql", response)
 
     if (!user) {
         throw Error("bad response")
@@ -13,8 +33,13 @@ const getFeedsFromResponse = (response = {}) => {
             edge_owner_to_timeline_media: photos,
         } = user
 
-        console.log("user.video", videos.user)
+        console.log("user.video", videos)
         console.log(photos.user)
+
+        console.log("user", user)
+
+
+
 
     }
 }
@@ -32,12 +57,13 @@ export const fetchData = async (username, numberOfFeeds) => {
     console.log("responser", response)
     feeds = getFeedsFromResponse(response);
 
-
-    console.log(Number(numberOfFeeds));
+    console.log("feeds", feeds)
+    console.log("number ofFeeds", Number(numberOfFeeds));
 
 
     if (Number(numberOfFeeds) < feeds.length) {
         feeds = feeds.slice(0, numberOfFeeds);
     }
+    return feeds
 
 }
